4405b7bc1a9661d8b131fff6b24559b88984431e
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-07-07 16:18:12 +0200

    removed sde code

--- security-flaws/src/main/java/com/example/securityflaws/ba/BrokenAuthenticationController.java
+++ security-flaws/src/main/java/com/example/securityflaws/ba/BrokenAuthenticationController.java
@@ -1,36 +1,13 @@
 package com.example.securityflaws.ba;
 
-import com.example.securityflaws.common.api.dto.CustomerDTO;
-import com.example.securityflaws.common.api.dto.CustomerMapper;
-import com.example.securityflaws.common.api.exception.NotFoundException;
-import com.example.securityflaws.common.customer.CustomerController;
-import com.example.securityflaws.common.customer.CustomerService;
-import io.vavr.collection.Seq;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.UUID;
-
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/flaws/ba/customers")
+@RequestMapping("/flaws/ba")
 @RequiredArgsConstructor
 public class BrokenAuthenticationController {
 
-    CustomerController customerController;
 
-    @GetMapping("{id}")
-    public CustomerDTO get(@PathVariable UUID id) {
-        return customerController.get(id);
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public CustomerDTO create(@RequestBody CustomerDTO dto, HttpServletResponse response) {
-        return customerController.create(dto, response);
-    }
 }
